d664d1907fbbeb1aad88639fdba3c315d8164017
Yashwitha192020013 <114204448+Yashwitha192020013@users.noreply.github.com>
2023-07-17 09:06:13 +0530

    Add files via upload

---
+++ Convert int into string then to num.py
@@ -0,0 +1,14 @@
+l=[1,2,3]
+str1=""
+for i in l:
+    str1=str1+str(i)
+print(str1)
+num=int(str1)
+print(num)
+result=num+1
+print(result)
+result=str(result)
+final=[]
+for i in result:
+    final.append(i)
+print(final)
